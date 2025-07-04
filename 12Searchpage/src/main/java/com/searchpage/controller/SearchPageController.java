83c5cb6a7230b6eefa422eaf5bc7f45ca6fa7d6c
Blesscode <iishaa.gupta@gmail.com>
2025-07-04 17:40:29 +0530

    display page ui done export section under development

--- 12Searchpage/src/main/java/com/searchpage/controller/SearchPageController.java
+++ 12Searchpage/src/main/java/com/searchpage/controller/SearchPageController.java
@@ -1,5 +1,7 @@
 package com.searchpage.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.searchpage.Binding.SearchRequest;
+import com.searchpage.entity.DataEntity;
 import com.searchpage.service.SearchServiceImplementation;
 
 @Controller
@@ -23,18 +26,37 @@ public class SearchPageController {
 		//add empty obj for form binding
 		SearchRequest request = new SearchRequest();
 		model.addAttribute("data", request);
-		model.addAttribute("statusList", service.statusOptions());
+		initStatus(model);
 		
 		return "SearchPage";
+	}
+
+
+	private void initStatus(Model model) {
+		model.addAttribute("statusList", service.statusOptions());
 	}
 	
 	
 	//2. get search req and send search response on ui page
 	@PostMapping("/search")
-	@ResponseBody
-	public String getObject(Model model,SearchRequest searchreq) {
+	public String searchResult(Model model,SearchRequest searchreq) {
+		//obj detail
 		System.out.println(searchreq);
-		return "Hello";
+		//based on obj details search
+		List<DataEntity> record = service.searchRecord(searchreq);
+		//test incoming data
+						for(DataEntity i:record) {
+							System.out.println(i);
+						}
+		//add the list of obj to the display ui
+		model.addAttribute("data", searchreq);
+		model.addAttribute("display",record);
+		//  Clear form by sending a new empty object to the form
+	    model.addAttribute("data", new SearchRequest());
+		//return home page ui
+		initStatus(model);
+		return "SearchPage";
+
 	}
 	
 	
--- 12Searchpage/src/main/java/com/searchpage/service/SearchServiceImplementation.java
+++ 12Searchpage/src/main/java/com/searchpage/service/SearchServiceImplementation.java
@@ -2,7 +2,9 @@ package com.searchpage.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.searchpage.Binding.SearchRequest;
@@ -25,7 +27,43 @@ public class SearchServiceImplementation implements SearchService {
 	@Override
 	public List<DataEntity> searchRecord(SearchRequest search) {
 		// TODO Auto-generated method stub
-		return null;
+		//1. based on the DYNAMIC obj req recive findAll records
+		/*
+		 * FOR DYNAMIC QUERY BASED ON PARAMETERS :  use QBE query by example
+		 * Example.of() only works with entity classes
+		 * Example is a wrapper object used to define search criteria dynamically using an entity object 
+		 * provide a partially filled entity, and Spring Data will query the database for all entities that match the non-null fields of that objec
+		 */
+		//copy binding to entity
+		DataEntity entity=new DataEntity();
+		//BeanUtils.copyProperties(search, entity); copies empty feild also which will result in query searching for empty feilds also
+		
+		//manual copy
+		//name
+		if(search.getName()!=null && !search.getName().trim().isEmpty()) {
+			entity.setName(search.getName());
+		}
+		//status
+		if(search.getStatus()!=null && !search.getStatus().trim().isEmpty()) {
+			entity.setStatus(search.getStatus());
+		}
+		//gender
+		if(search.getGender()!=null && !search.getGender().trim().isEmpty()) {
+			entity.setGender(search.getGender());
+		}
+		//start-date
+		if(search.getStartDate()!=null ) {
+			entity.setStart(search.getStartDate());
+		}
+		//end-date
+		if(search.getEndDate()!=null ) {
+			entity.setEnd(search.getEndDate());
+		}
+
+		
+		List<DataEntity> listRecords = repo.findAll(Example.of(entity));
+
+		return listRecords;
 	}
 
 	@Override
