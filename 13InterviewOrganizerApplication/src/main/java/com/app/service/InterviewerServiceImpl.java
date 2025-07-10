09743f51c0e257b6079d5875cb95337202c83312
Blesscode <iishaa.gupta@gmail.com>
2025-07-10 06:24:24 +0530

    Home Page Ui under development

---
+++ 13InterviewOrganizerApplication/src/main/java/com/app/service/InterviewerServiceImpl.java
@@ -0,0 +1,45 @@
+package com.app.service;
+
+import com.app.binding.LoginPageBinding;
+import com.app.binding.RegisterPageBinding;
+import com.app.binding.UnlockPageBinding;
+
+public class InterviewerServiceImpl implements InterviewerService {
+
+	@Override
+	public Boolean registerNewInterviewer(RegisterPageBinding register) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean findInterviewer(LoginPageBinding login) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean activateAccount(UnlockPageBinding unlock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean sendPwdToEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean sendActivationPwdToEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean lockActivationLink() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ 13InterviewOrganizerApplication/src/main/resources/static/script.js
@@ -0,0 +1,19 @@
+
+gsap.from(".animateText>h2",{
+
+    opacity:0,
+    x:-20,
+    duration:2,
+    delay:1,
+    stagger:1    /*element comes one after other */
+})
+
+// gsap.from(".page2>.cards-container",{
+
+//    transform: "translateX(150px)",
+//   stagger: 0.2, // delay between each card
+//   duration:2,
+// repeat:-1,
+// yoyo:"true"
+
+// })
